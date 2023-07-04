8600a983e8a2246deb9efdcc7a876986d978e2f2
Ainal <ainal1haque@gmail.com>
2023-07-04 21:16:01 +0600

    update rest api with db

---
+++ Lession10_restapiwithmongo/app.js
@@ -0,0 +1,29 @@
+const express= require('express');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+const app= express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+//Home page
+app.get("/",(req,res)=>{
+    res.sendFile (__dirname+"/views/index.html");   
+});
+
+//route not found
+app.use((req,res,next)=>{
+    res.status(404).json({message:"route not Found"});
+});
+
+// server Error
+
+app.use((err,req,res,next)=>{
+    res.status(500).json({message:"Something Went to Wrong!!"});
+});
+
+
+
+module.exports=app;
---
+++ Lession10_restapiwithmongo/index.js
@@ -0,0 +1,8 @@
+const app =require("./app");
+
+const PORT=5000;
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
